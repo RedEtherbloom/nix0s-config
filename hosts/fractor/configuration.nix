61936c6ab80de6240781ed3556e103f2c47c5e8f
RedEtherbloom <etherbloom+github@mailbox.org>
2024-10-02 14:19:22 +0200

    feat: Add restic backup to fractor

--- hosts/fractor/configuration.nix
+++ hosts/fractor/configuration.nix
@@ -1,4 +1,5 @@
 {
+  config,
   lib,
   pkgs,
   ...
@@ -152,7 +153,7 @@
       "lp"
     ];
     packages = with pkgs; [
-      aircrack-ng   
+      aircrack-ng
     ];
   };
 
@@ -223,6 +224,46 @@
 
   # Android
   programs.adb.enable = true;
+
+  # TODO: Reread secrets managment with yaml
+  sops.secrets."resticPassword" = {
+    format = "binary";
+    sopsFile = ../../secrets/fractor/restic/resticPassword;
+  };
+  sops.secrets."resticRestOptions" = {
+    format = "binary";
+    sopsFile = ../../secrets/fractor/restic/restTransportPassword;
+  };
+  sops.secrets."resticExcludeFile" = {
+    format = "binary";
+    sopsFile = ../../secrets/fractor/restic/backup.exclude;
+  };
+  services.restic.backups."root" = {
+    timerConfig = {
+      OnCalendar = "wednesday, friday, sunday 22:00";
+      Persistent = true;
+    };
+    # TODO: Generate Certfile and rollout #security
+    # TODO: Reference neurodrive port
+    repository = "rest:http://192.168.178.56:8193/infinity-fractor/";
+    pruneOpts = [
+      "--keep-daily 3"
+      "--keep-weekly 5"
+      "--keep-monthly 10"
+      "--keep-yearly 50"
+    ];
+    paths = [ "/" ];
+    passwordFile = config.sops.secrets."resticPassword".path;
+    inhibitsSleep = true;
+    extraBackupArgs = [
+      "--exclude-caches"
+      "--exclude-file ${config.sops.secrets.resticExcludeFile.path}"
+    ];
+    environmentFile = config.sops.secrets."resticRestOptions".path;
+    createWrapper = true;
+    # TODO: Write script that blocks if the wifi is a hotspot
+    #backupPrepareCommand = ;
+  };
 
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
