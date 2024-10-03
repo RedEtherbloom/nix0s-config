9e201856893affc8c86eed4e8bf8006aa0a9a58f
RedEtherbloom <etherbloom+github@mailbox.org>
2024-10-03 12:32:03 +0200

    refactor: Split off restic

--- hosts/fractor/configuration.nix
+++ hosts/fractor/configuration.nix
@@ -1,11 +1,12 @@
 {
-  config,
   lib,
   pkgs,
   ...
 }:
 {
   imports = [
+    ./restic.nix 
+
     ../../modules/common/default.nix
     ../../modules/wireguard/default.nix
 
@@ -146,46 +147,6 @@
   };
   environment.sessionVariables = {
     LIBVA_DRIVER_NAME = "i965";
-  };
-
-  # TODO: Reread secrets managment with yaml
-  sops.secrets."resticPassword" = {
-    format = "binary";
-    sopsFile = ../../secrets/fractor/restic/resticPassword;
-  };
-  sops.secrets."resticRestOptions" = {
-    format = "binary";
-    sopsFile = ../../secrets/fractor/restic/restTransportPassword;
-  };
-  sops.secrets."resticExcludeFile" = {
-    format = "binary";
-    sopsFile = ../../secrets/fractor/restic/backup.exclude;
-  };
-  services.restic.backups."root" = {
-    timerConfig = {
-      OnCalendar = "wednesday, friday, sunday 22:00";
-      Persistent = true;
-    };
-    # TODO: Generate Certfile and rollout #security
-    # TODO: Reference neurodrive port
-    repository = "rest:http://192.168.178.56:8193/infinity-fractor/";
-    pruneOpts = [
-      "--keep-daily 3"
-      "--keep-weekly 5"
-      "--keep-monthly 10"
-      "--keep-yearly 50"
-    ];
-    paths = [ "/" ];
-    passwordFile = config.sops.secrets."resticPassword".path;
-    inhibitsSleep = true;
-    extraBackupArgs = [
-      "--exclude-caches"
-      "--exclude-file ${config.sops.secrets.resticExcludeFile.path}"
-    ];
-    environmentFile = config.sops.secrets."resticRestOptions".path;
-    createWrapper = true;
-    # TODO: Write script that blocks if the wifi is a hotspot
-    #backupPrepareCommand = ;
   };
 
   # Before changing this value read the documentation for this option
