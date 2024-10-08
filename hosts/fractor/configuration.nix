eb629bd82968c3a49ee6592cc620a478d295efdc
RedEtherbloom <etherbloom+github@mailbox.org>
2024-10-09 00:32:05 +0200

    fix: Use intel hybrid driver to resolve a conflict with some service adding one with hybrid enabled

--- hosts/fractor/configuration.nix
+++ hosts/fractor/configuration.nix
@@ -123,6 +123,9 @@
     openFirewall = true;
   };
 
+  nixpkgs.config.packageOverrides = pkgs: {
+    intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
+  };  
   # IntelGPU Hardware Acceleration
   hardware.graphics = {
     enable = true;
