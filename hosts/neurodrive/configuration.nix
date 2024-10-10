972295b0673b03c7f026acd66600f2e3c6e23697
RedEtherbloom <etherbloom+github@mailbox.org>
2024-10-11 01:41:17 +0200

    fix: Disable open kernel nvidia kernel module(vaapi conflict)

--- hosts/neurodrive/configuration.nix
+++ hosts/neurodrive/configuration.nix
@@ -163,8 +163,32 @@ in
   ];
 
   environment.sessionVariables = {
+    #LIBVA_DRIVER_NAME = "nvidia";
+    #MOZ_DISABLE_RDD_SANDBOX = "1";
+
+    # Necessary to correctly enable va-api (video codec hardware
+    # acceleration). If this isn't set, the libvdpau backend will be
+    # picked, and that one doesn't work with most things, including
+    # Firefox.
     LIBVA_DRIVER_NAME = "nvidia";
+    # Required to run the correct GBM backend for nvidia GPUs on wayland
+    GBM_BACKEND = "nvidia-drm";
+    # Apparently, without this nouveau may attempt to be used instead
+    # (despite it being blacklisted)
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    # Hardware cursors are currently broken on nvidia
+    WLR_NO_HARDWARE_CURSORS = "1";
+
+    # Required to use va-api it in Firefox. See
+    # https://github.com/elFarto/nvidia-vaapi-driver/issues/96
     MOZ_DISABLE_RDD_SANDBOX = "1";
+    # It appears that the normal rendering mode is broken on recent
+    # nvidia drivers:
+    # https://github.com/elFarto/nvidia-vaapi-driver/issues/213#issuecomment-1585584038
+    NVD_BACKEND = "direct";
+    # Required for firefox 98+, see:
+    # https://github.com/elFarto/nvidia-vaapi-driver#firefox
+    EGL_PLATFORM = "wayland";
   };
 
   services.avahi = {
@@ -210,7 +234,10 @@ in
     # Currently alpha-quality/buggy, so false is currently the recommended setting.
     #
     # May have improved now
-    open = true;
+    # 
+    # Incompability with vaapi-driver
+    # See: https://github.com/elFarto/nvidia-vaapi-driver/issues/312
+    open = false;
   };
 
   sops.secrets."restic_server/private_certificate" = {
