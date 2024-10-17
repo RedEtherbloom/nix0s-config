ba65be715f3105d6b9eaa7009460157556768cf0
RedEtherbloom <etherbloom+github@mailbox.org>
2024-10-18 01:14:14 +0200

    fix: Add missing gaming role to fractor

--- flake.nix
+++ flake.nix
@@ -79,6 +79,7 @@
           modules = [
             ./hosts/fractor/configuration.nix
             ./modules/laptop.nix
+            ./modules/gaming.nix
             {
               home-manager.users.inf.imports = [
                 { home.stateVersion = "24.05"; }
