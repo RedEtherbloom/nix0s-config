4329cc3daa8c5a92f17db46efb6af945a5ed0df0
RedEtherbloom <etherbloom+github@mailbox.org>
2024-10-25 23:00:05 +0200

    feat: Add audiosink service for fritz-logger

---
+++ homeManagerModules/hosts/audiosink/default.nix
@@ -0,0 +1,5 @@
+{ ... }: {
+  imports =  [
+    ./fritz-logger-service.nix
+  ];
+}
---
+++ homeManagerModules/hosts/audiosink/fritz-logger-service.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, ... }:
+{
+  sops.secrets.fritz_box_password = {
+    sopsFile = ../../../secrets/services/fritz-logger.yaml;
+  };
+
+  systemd.user.services.fritz-logger = {
+    Unit = {
+      Description = "Periodically logs connected fritz box clients to a sqlite database";
+    };
+    Install = {
+      # May need default.target tbh
+      WantedBy = [ "multi-user.target" ];
+    };
+    Service = {
+      Type = "simple";
+      After = [
+        "network.target"
+        "sops-nix.service"
+      ];
+      ExecStart = "${pkgs.fritz-logger}/bin/fritz-logger";
+      Environment = {
+        IP_ADDRESS = "192.168.178.1";
+        PASSWORD_FILE = config.sops.secrets."fritz-logger".password.path;
+        PRUNE_OLDER_THAN = 14;
+        QUERY_PERIOD = 15 * 60;
+      };
+    };
+  };
+}
