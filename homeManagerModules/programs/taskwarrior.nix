aebdfd1292f63322a178e989af35210db2759fd7
RedEtherbloom <etherbloom+github@mailbox.org>
2024-10-29 17:00:26 +0100

    stub: Add current work so that I can push it

---
+++ homeManagerModules/programs/taskwarrior.nix
@@ -0,0 +1,30 @@
+{
+  config,
+  osConfig,
+  ...
+}:
+{
+  # TODO: How do I group these?
+  sops.secrets.encryption_secret = {
+    sopsFile = "../../secrets/services/taskwarrior.yaml";
+  };
+  sops.secrets.client_id = {
+    sopsFile = "../../secrets/services/taskwarrior.yaml";
+  };
+
+  services.taskwarrior-sync.enable = true;
+
+  programs.taskwarrior = {
+    enable = true;
+    config = {
+      # TODO: Insert taskrc from fractor
+      sync = {
+        encryption_secret = "!${config.sops.secrets.encryption_secret.path}";
+        server = {
+          url = with osConfig.myOptions.taskchampion; "http://${taskchampionIP}:${taskchampionPort}";
+          client_id = "!${config.sops.secrets.client_id.path}";
+        };
+      };
+    };
+  };
+}
