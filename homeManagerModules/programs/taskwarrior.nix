1c4663b7ca99f1c3001f348ce936daadb08cbc46
RedEtherbloom <etherbloom+github@mailbox.org>
2024-11-01 23:09:21 +0100

    taskwarrior: refactor for readability and docs

--- homeManagerModules/programs/taskwarrior.nix
+++ homeManagerModules/programs/taskwarrior.nix
@@ -3,15 +3,19 @@
   osConfig,
   ...
 }:
+let
+  taskwarrior_secrets = ../../secrets/services/taskwarrior.yaml;
+in
 {
   # TODO: How do I group these?
   sops.secrets.encryption_secret = {
-    sopsFile = ../../secrets/services/taskwarrior.yaml;
+    sopsFile = taskwarrior_secrets;
   };
   sops.secrets.client_id = {
-    sopsFile = ../../secrets/services/taskwarrior.yaml;
+    sopsFile = taskwarrior_secrets;
   };
 
+  # TODO: Can other users read this by default as well? I hope not
   sops.templates."taskwarrior-sync.rc".content = ''
     sync.encryption_secret = "${config.sops.placeholder.encryption_secret}"
     sync.server.client_id = "${config.sops.placeholder.client_id}""
