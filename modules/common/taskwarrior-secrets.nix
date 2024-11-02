62a97b5201d0bfecadde0ed0ee31df59475b1c4b
RedEtherbloom <etherbloom+github@mailbox.org>
2024-11-02 13:23:06 +0100

    taskwarrior: Move to nixos so that we can use sops-nix templates

---
+++ modules/common/taskwarrior-secrets.nix
@@ -0,0 +1,26 @@
+# TODO: Move to home-managr once it supports sops-nix templates
+{ config, ... }:
+let
+  functions = import ./functions.nix { inherit config; };
+  taskwarrior_secrets = ../../secrets/services/taskwarrior.yaml;
+in
+{
+  # TODO: How do I group these?
+  sops.secrets.encryption_secret = {
+    sopsFile = taskwarrior_secrets;
+  };
+  sops.secrets.client_id = {
+    sopsFile = taskwarrior_secrets;
+  };
+
+  # TODO: Can other users read this by default as well? I hope not
+  # TODO: Use vars.data-server-ip once we have refactored variables.nix
+  sops.templates."taskwarrior-sync.rc" = {
+    owner = "inf";
+    content = ''
+    sync.encryption_secret = "${config.sops.placeholder.encryption_secret}"
+    sync.server.client_id = "${config.sops.placeholder.client_id}" 
+    sync.server.url = "http://${functions.data-server-ip}:${toString config.myOptions.taskchampion.taskchampionPort}"
+  '';
+  };
+}
