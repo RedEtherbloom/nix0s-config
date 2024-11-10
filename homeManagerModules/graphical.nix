83bac3f0a2071fca36db51912210b980f4f3d66f
RedEtherbloom <etherbloom+github@mailbox.org>
2024-11-10 18:08:03 +0100

    stylix: Remove file blocking rebuild

--- homeManagerModules/graphical.nix
+++ homeManagerModules/graphical.nix
@@ -4,4 +4,9 @@
     inputs.plasma-manager.homeManagerModules.plasma-manager
     ./base.nix
   ];
+
+  # Remove files for stylix
+  lib.hm.dag.entryAfter ["writeBoundary"] ''
+    run rm -rf ~/.config/gtk-3.0 ~/.config/gtk-4.0 ~/.gtkrc-2.0
+  '';
 }
