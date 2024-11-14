9e54ad61b52db6df0c4bd5ccd83450798cbc1e99
RedEtherbloom <etherbloom+github@mailbox.org>
2024-11-14 21:24:34 +0100

    stylix: Upscale background for neurodrive

--- modules/graphical.nix
+++ modules/graphical.nix
@@ -6,9 +6,10 @@
     ./base.nix
   ];
 
+  # TODO: This should really be per device and potentially stored out-of-this-repo
   stylix = {
     enable = true;
-    image = ../dotfiles/wallpaper/catgirl.jpg;
+    image = ../dotfiles/wallpaper/catgirl_waifu2x_noise2_scale4x.png;
     polarity = "dark";
   };
 }
