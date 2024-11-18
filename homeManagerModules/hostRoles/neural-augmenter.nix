f8350ff4efd5eef6bbf9fe8518afbec66200d3d9
RedEtherbloom <etherbloom+github@mailbox.org>
2024-11-18 23:31:50 +0100

    taskwarrior-tui: Turn into nix option

--- homeManagerModules/hostRoles/neural-augmenter.nix
+++ homeManagerModules/hostRoles/neural-augmenter.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 {
   imports = [
     ./graphical.nix
@@ -8,5 +9,30 @@
     enable = true;
     enableSync = true;
     taskopen = true;
+  };
+  myOptions.taskwarrior-tui = {
+    enable = true;
+    package =
+      with pkgs;
+      (taskwarrior-tui.overrideAttrs (oldAttrs: rec {
+        version = oldAttrs.version + "-fix";
+
+        src = (
+          pkgs.fetchFromGitHub {
+            owner = "RedEtherbloom";
+            repo = "taskwarrior-tui";
+            hash = "sha256-YNd4vtaWm+1fsB8ly3toq2u74Nicmhx2ey1m557q4K8=";
+            rev = "ee24bfb4a36f246933e6d2502ab85d3fc6abb85b";
+          }
+        );
+
+        cargoDeps = oldAttrs.cargoDeps.overrideAttrs (
+          lib.const {
+            name = "taskwarrior-tui-vendor.tar.gz";
+            inherit src;
+            outputHash = "sha256-jtVUXWVrBq6xS4y9HKz+JtXHc6LvIk0cC7xmiPB1+ro=";
+          }
+        );
+      }));
   };
 }
--- modules/common/base_pkgs.nix
+++ modules/common/base_pkgs.nix
@@ -120,26 +120,7 @@ in
         thunderbird
         kate
         kdePackages.kalk
-        (taskwarrior-tui.overrideAttrs (oldAttrs: rec {
-          version = oldAttrs.version + "-fix";
-
-          src = (
-            pkgs.fetchFromGitHub {
-              owner = "RedEtherbloom";
-              repo = "taskwarrior-tui";
-              hash = "sha256-YNd4vtaWm+1fsB8ly3toq2u74Nicmhx2ey1m557q4K8=";
-              rev = "ee24bfb4a36f246933e6d2502ab85d3fc6abb85b";
-            }
-          );
-
-          cargoDeps = oldAttrs.cargoDeps.overrideAttrs (
-            lib.const {
-              name = "taskwarrior-tui-vendor.tar.gz";
-              inherit src;
-              outputHash = "sha256-jtVUXWVrBq6xS4y9HKz+JtXHc6LvIk0cC7xmiPB1+ro=";
-            }
-          );
-        }))
+        
         bitwarden
         bitwarden-cli
         wl-clipboard
