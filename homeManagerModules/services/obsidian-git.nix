f205a359dd3d675e486f7389fd1282d32244c8aa
RedEtherbloom <etherbloom+github@mailbox.org>
2024-11-22 16:15:08 +0100

    obsidian-git: Finally fix whitespace bug

--- homeManagerModules/services/obsidian-git.nix
+++ homeManagerModules/services/obsidian-git.nix
@@ -5,12 +5,11 @@
   ...
 }:
 with lib; let
-  cfg = config.myOptions.obsidian-git;
+  cfg = config.myOptions.obsidian;
   vars = import ../variables.nix {inherit config osConfig;};
 
   # A hour sounds like a good preset
   DEFAULT_SYNC_INTERVAL = 3600;
-  # Spaces in name blocked by this: https://github.com/simonthum/git-sync/issues/33
   OBSIDIAN_VAULTS = "Obsidian";
 
   generate_vault_dir_path = config.xdg.userDirs.documents + "/" + OBSIDIAN_VAULTS;
@@ -21,9 +20,7 @@ with lib; let
     uri = "ssh://${config.home.username}@${vars.data-server-ip}${vars.own-hm-data-directory}/obsidian-vaults/${vault-name}";
   };
 in {
-  options.myOptions.obsidian-git.enable = mkEnableOption "obsidian vault git sync";
-
-  config = mkIf cfg.enable {
+  config = mkIf cfg.enableSync {
     services.git-sync = {
       enable = true;
       repositories = {
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,4 +1,14 @@
 final: prev: {
   fritz-logger = prev.callPackage ./scripts/python/fritz-logger/default.nix {};
   byar-launcher = prev.callPackage ./byar-launcher.nix {};
+
+  # Current fix
+  git-sync = prev.git-sync.overrideAttrs {
+    src = prev.fetchFromGitHub {
+      owner = "RedEtherbloom";
+      repo = "git-sync";
+      rev = "68822d725e57b6875c13fe27076a18f235186a8e";
+      hash = "sha256-t1NVgp+ELmTMK0N1fFFJCoKQd8mSYSMAIDG9+kNs3Ok=";
+    };
+  };
 }
