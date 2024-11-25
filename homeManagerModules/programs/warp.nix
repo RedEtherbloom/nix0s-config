ef5d5489c209c2c18fec7a19231cd6592723d151
RedEtherbloom <etherbloom+github@mailbox.org>
2024-11-25 21:44:06 +0100

    warp: set proper gst flags

--- homeManagerModules/programs/warp.nix
+++ homeManagerModules/programs/warp.nix
@@ -2,6 +2,7 @@
   config,
   lib,
   osConfig,
+  pkgs,
   ...
 }:
 with lib; let
@@ -16,6 +17,11 @@ in {
   };
 
   config = mkIf cfg.enable {
-    home.packages = [pkgs.warp];
+    home.packages = [
+      (pkgs.warp.overrideAttrs
+        (oldAttrs: {
+          buildInputs = oldAttrs.buildInputs ++ [(pkgs.gst_all_1.gst-plugins-bad.override {enableZbar = true;})];
+        }))
+    ];
   };
 }
