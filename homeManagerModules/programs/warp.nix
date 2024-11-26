d1921b762b800d49c8a382e6a2c79f70bf4f164e
RedEtherbloom <etherbloom+github@mailbox.org>
2024-11-26 15:15:54 +0100

    warp: revert on main branch

--- homeManagerModules/programs/warp.nix
+++ homeManagerModules/programs/warp.nix
@@ -18,10 +18,7 @@ in {
 
   config = mkIf cfg.enable {
     home.packages = [
-      (pkgs.warp.overrideAttrs
-        (oldAttrs: {
-          buildInputs = oldAttrs.buildInputs ++ [(pkgs.gst_all_1.gst-plugins-bad.override {enableZbar = true;})];
-        }))
+      pkgs.warp
     ];
   };
 }
