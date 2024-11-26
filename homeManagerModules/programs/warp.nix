60be4a2c25f6612e1ec3b1a309b629976f022d0d
RedEtherbloom <etherbloom+github@mailbox.org>
2024-11-26 15:15:09 +0100

    warp: attempt fix(gets further)

--- homeManagerModules/programs/warp.nix
+++ homeManagerModules/programs/warp.nix
@@ -18,7 +18,18 @@ in {
 
   config = mkIf cfg.enable {
     home.packages = [
-      pkgs.warp
+      (pkgs.warp.overrideAttrs
+        (oldAttrs: {
+          buildInputs = oldAttrs.buildInputs ++ [pkgs.gst_all_1.gst-plugins-good (pkgs.gst_all_1.gst-plugins-bad.override {enableZbar = true;})];
+          nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [pkgs.gst_all_1.gstreamer];
+
+          preFixup = ''
+            gappsWrapperArgs+=(
+              # vp8enc preset
+              --prefix GST_PRESET_PATH : "${pkgs.gst_all_1.gst-plugins-good}/share/gstreamer-1.0/presets"
+            )
+          '';
+        }))
     ];
   };
 }
