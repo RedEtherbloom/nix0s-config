ab1e05f12c389c021a4db60692903d40fc9a3949
RedEtherbloom <etherbloom+github@mailbox.org>
2024-12-08 17:11:59 +0100

    stylix: move to neural-augmenter(for now)

--- modules/hostRoles/graphical.nix
+++ modules/hostRoles/graphical.nix
@@ -1,38 +1,14 @@
 {
   config,
   lib,
-  inputs,
-  pkgs,
   ...
 }:
 with lib; let
   cfg = config.myOptions.hostRoles.graphical;
 in {
-  imports = [
-    inputs.stylix.nixosModules.stylix
-  ];
-
   options.myOptions.hostRoles.graphical.enable = mkEnableOption "graphical session settings";
 
   config = mkIf cfg.enable {
     myOptions.hostRoles.base.enable = mkDefault true;
-
-    stylix = {
-      enable = true;
-      polarity = "light";
-      targets.grub.useImage = true;
-    };
-
-    # Open the ports for KDE-Connect and install it here as well.
-    # HM can't open ports sadly.
-    programs.kdeconnect = {
-      enable = true;
-      package = mkForce pkgs.kdePackages.kdeconnect-kde;
-    };
-
-    # For some reason this keeps getting pulled in since stlix and then recycled by ghc
-    environment.systemPackages = with pkgs; [
-      ghc
-    ];
   };
 }
