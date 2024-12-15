a0797eb16576ec0f8037ed1cb17f54b4c473be36
RedEtherbloom <etherbloom+github@mailbox.org>
2024-12-16 00:15:29 +0100

    byar: rewrite git path for caching

--- pkgs/byar-launcher.nix
+++ pkgs/byar-launcher.nix
@@ -1,12 +1,7 @@
-{ pkgs }: let
+{
+  inputs,
+  pkgs,
+}: let
   # TODO: How is this license wise?
-  byar-launcher =
-    pkgs.fetchFromGitHub {
-      owner = "sergv";
-      repo = "nixos-config";
-      rev = "9c6306c86af6130f76d277e382c346360ec124dd";
-      sha256 = "sha256-FazhyLRMmg7A62SYgF/+h3AXnl0CNxElVfCp21cfsYY=";
-    }
-    + "/beyond-all-reason-launcher.nix";
-in
-  (pkgs.callPackage byar-launcher {})
+  byar-launcher = "${inputs.sergv-nixos-config}/beyond-all-reason-launcher.nix";
+in (pkgs.callPackage byar-launcher {})
