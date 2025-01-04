89c2c383e45900b98b22b698e0b9fc7e9b70fd08
RedEtherbloom <etherbloom+github@mailbox.org>
2025-01-04 18:37:26 +0100

    starsector: move override to neurodrive

--- homeManagerModules/roles/gaming.nix
+++ homeManagerModules/roles/gaming.nix
@@ -20,16 +20,7 @@ in {
       gcs
       byar-launcher
       rimsort
-      # Eve: Account for bug: https://fractalsoftworks.com/forum/index.php?topic=30633.0
-      (starsector.overrideAttrs (oldAttrs: {
-        buildInputs = oldAttrs.buildInputs ++ [pkgs.makeWrapper];
-
-        postInstall =
-          (oldAttrs.postInstall or "")
-          + ''
-            wrapProgram "$out/bin/starsector" --set __GL_THREADED_OPTIMIZATIONS 0
-          '';
-      }))
+      starsector
 
       dxvk_2
     ];
