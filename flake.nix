7f41c6504e970c42da023e4c0e0117bd21d73abd
RedEtherbloom <etherbloom+github@mailbox.org>
2025-01-15 10:57:09 +0100

    neurodrive: install libretranslate

--- flake.nix
+++ flake.nix
@@ -85,7 +85,7 @@
   } @ inputs: let
     nixpkgsConfig = {
       overlays = [
-        (import ./pkgs {inherit inputs;})
+        (import ./pkgs {inherit inputs; inherit (nixpkgs) lib;})
         nix-vscode-extensions.overlays.default
         nix-comfyui.overlays.default
       ];
