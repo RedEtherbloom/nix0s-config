8e2785ba4b8df82619af31ed8817db5a462363cb
RedEtherbloom <etherbloom+github@mailbox.org>
2025-01-27 15:10:40 +0100

    neurodrive: fix error in layout

--- hosts/neurodrive/home.nix
+++ hosts/neurodrive/home.nix
@@ -64,10 +64,11 @@ in {
     }
     # Ivy: These sadly break with missing monitors :/
     (generate-kscreen-doctor "screen-benq" "kscreen-doctor output.DP-3.disable output.HDMI-A-1.disable output.DP-2.enable output.DP-2.rotation.normal output.DP-2.position.0,0" "ctrl+shift+f1")
-    (generate-kscreen-doctor "screen-dual" "kscreen-doctor output.HDMI-A-1.disable output.DP-2.enable output.DP-2.position.1080,420 output.DP-2.priority.1 output.DP-2.rotation.right output.DP-3.enable output.DP-3.rotation.inverted output.DP-3.position.0,0" "ctrl+shift+f2")
+    (generate-kscreen-doctor "screen-dual" "kscreen-doctor output.HDMI-A-1.disable output.DP-2.enable output.DP-2.position.1080,420 output.DP-2.priority.1 output.DP-2.rotation.normal output.DP-3.enable output.DP-3.rotation.right output.DP-3.position.0,0" "ctrl+shift+f2")
     (generate-kscreen-doctor "screen-aoc" "kscreen-doctor output.HDMI-A-1.disable output.DP-3.enable output.DP-3.position.0,0 output.DP-3.rotation.right output.DP-2.disable" "ctrl+shift+f3")
     (generate-kscreen-doctor "screen-lg" "kscreen-doctor output.DP-2.disable output.DP-3.disable output.HDMI-A-1.enable output.HDMI-A-1.position.0,0" "ctrl+shift+f4")
     (generate-kscreen-doctor "screen-lg-aoc" "kscreen-doctor output.DP-2.disable output.HDMI-A-1.enable output.HDMI-A-1.position.1080,420 output.HDMI-A-1.priority.1 output.DP-3.enable output.DP-3.position.0,0 output.DP-3.rotation.right" "ctrl+shift+f5")
-    (generate-kscreen-doctor "screen-trial" "kscreen-doctor output.DP-2.enable output.DP-2.position.1080,420 output.DP-2.priority.1 output.DP-2.rotation.normal output.DP-3.enable output.DP-3.rotation.right output.DP-3.position.0,0 output.DP-3.priority.2 output.HDMI-A-1.enable output.HDMI-A-1.position.3000,420 output.HDMI-A-1.rotation.normal output.HDMI-A-1.priority.3" "ctrl+shift+f6")
+    (generate-kscreen-doctor "screen-lg-benq" "kscreen-doctor output.DP-2.enable output.DP-2.position.0,0 output.DP-2.priority.1 output.DP-2.rotation.normal output.DP-3.disable output.HDMI-A-1.enable output.HDMI-A-1.position.1920,0 output.HDMI-A-1.rotation.normal output.HDMI-A-1.priority.2" "ctrl+shift+f6")
+    (generate-kscreen-doctor "screen-trial" "kscreen-doctor output.DP-2.enable output.DP-2.position.1080,420 output.DP-2.priority.1 output.DP-2.rotation.normal output.DP-3.enable output.DP-3.rotation.right output.DP-3.position.0,0 output.DP-3.priority.2 output.HDMI-A-1.enable output.HDMI-A-1.position.3000,420 output.HDMI-A-1.rotation.normal output.HDMI-A-1.priority.3" "ctrl+shift+f7")
   ];
 }
