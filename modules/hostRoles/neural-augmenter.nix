f3268d574fc551c949cea3a272d4f3e780e8c5cf
RedEtherbloom <etherbloom+github@mailbox.org>
2025-02-06 15:37:48 +0100

    stylix: hotfix for plasma6 breaking due to Kvantum

--- modules/hostRoles/neural-augmenter.nix
+++ modules/hostRoles/neural-augmenter.nix
@@ -22,6 +22,8 @@ in {
       enable = true;
       polarity = "dark";
       targets.grub.useImage = true;
+      # Workaround for bug https://github.com/danth/stylix/issues/835
+      targets.qt.platform = "kde";
     };
 
     # Open the ports for KDE-Connect and install it here as well.
