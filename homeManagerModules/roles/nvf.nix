4da506c6fa32b7dd5b373840f6d9b3fa8d1ed41b
RedEtherbloom <etherbloom+github@mailbox.org>
2025-02-10 15:27:18 +0100

    nvf: enable multi-select for telescope

--- homeManagerModules/roles/nvf.nix
+++ homeManagerModules/roles/nvf.nix
@@ -33,17 +33,46 @@ in {
         vimAlias = true;
         # Redundant?
         lsp.enable = true;
-        binds.cheatsheet.nvim-cheatsheet.enable = true;
+        binds.cheatsheet.enable = true;
 
         # Copied from Vimjoyer
         statusline.lualine.enable = true;
         telescope = {
           enable = true;
-          path_display = "smart";
+          setupOpts.defaults = {
+            path_display = ["smart"];
+            mappings = ''
+              ["i"] = {
+                ["<CR>"] = utils.plugins.telescope_open_single_or_multi,
+              }
+            '';
+          };
         };
         autocomplete.nvim-cmp.enable = true;
 
         keymaps = [(mkKeymap "n" "<leader>fk" "<cmd>Telescope keymaps<CR>" {desc = "Open Telescopes built in keymap";})];
+        # Copied and modified from https://www.reddit.com/r/neovim/comments/1f7nkbe/comment/lle1nbc/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=sha
+        luaConfigRC.telescopeMultiSelect = ''
+          ---Telescope action helper to open single or multiple files
+          ---@param bufnr integer Telescope prompt buffer number
+          function Plugins.telescope_open_single_or_multi(bufnr)
+            local actions = require("telescope.actions")
+            local actions_state = require("telescope.actions.state")
+            local single_selection = actions_state.get_selected_entry()
+            local multi_selection = actions_state.get_current_picker(bufnr):get_multi_selection()
+            if not vim.tbl_isempty(multi_selection) then
+              actions.close(bufnr)
+              for _, file in pairs(multi_selection) do
+                if file.path ~= nil then
+                  vim.cmd(string.format("tabedit %s", file.path))
+                end
+              end
+              vim.cmd(string.format("edit %s", single_selection.path))
+            else
+              actions.select_default(bufnr)
+            end
+          end'';
+
         languages = {
           enableFormat = true;
           enableLSP = true;
@@ -85,37 +114,6 @@ in {
               };
             };
           };
-
-          # Copied and modified from https://www.reddit.com/r/neovim/comments/1f7nkbe/comment/lle1nbc/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=sha
-          luaConfigRC.telescopeMultiSelect = ''
-            ---Telescope action helper to open single or multiple files
-            ---@param bufnr integer Telescope prompt buffer number
-            function Plugins.telescope_open_single_or_multi(bufnr)
-              local actions = require("telescope.actions")
-              local actions_state = require("telescope.actions.state")
-              local single_selection = actions_state.get_selected_entry()
-              local multi_selection = actions_state.get_current_picker(bufnr):get_multi_selection()
-              if not vim.tbl_isempty(multi_selection) then
-                actions.close(bufnr)
-                for _, file in pairs(multi_selection) do
-                  if file.path ~= nil then
-                    vim.cmd(string.format("tabedit %s", file.path))
-                  end
-                end
-                vim.cmd(string.format("edit %s", single_selection.path))
-              else
-                actions.select_default(bufnr)
-              end
-            end'';
-          vim.telescope.setupOpts = ''
-            defaults = {
-              mappings = {
-                ["i"] = {
-                  ["<CR>"] = utils.plugins.telescope_open_single_or_multi,
-                }
-              }
-            }
-          '';
         };
       };
     };
