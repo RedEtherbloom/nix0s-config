03cd078c38b59fb407738bf50f13e4e1022ab4af
RedEtherbloom <etherbloom+github@mailbox.org>
2025-02-13 13:47:49 +0100

    git-sync: remove own patch
    Patch is now merged upstream.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -16,16 +16,6 @@ in {
   fritz-logger = final.callPackage ./scripts/python/fritz-logger/default.nix {};
   byar-launcher = final.callPackage "${inputs.sergv-nixos-config}/beyond-all-reason-launcher.nix" {};
 
-  # Current fix
-  git-sync = prev.git-sync.overrideAttrs {
-    src = prev.fetchFromGitHub {
-      owner = "simonthum";
-      repo = "git-sync";
-      rev = "7242291edf543ecc1bb9de8f47086bb69a5cb9f7";
-      hash = "sha256-t1NVgp+ELmTMK0N1fFFJCoKQd8mSYSMAIDG9+kNs3Ok=";
-    };
-  };
-
   python3 = let
     # Bug that has to be reported. Was blocking libretranslate build when CUDA enabled
     # Similar to https://github.com/NixOS/nixpkgs/pull/371640
