a6594b6571feb4647dc61776a298f27cbb6ee1be
RedEtherbloom <etherbloom+github@mailbox.org>
2025-02-22 13:23:02 +0100

    nvf: bump version

--- flake.nix
+++ flake.nix
@@ -86,7 +86,7 @@
     };
 
     nvf = {
-      url = "github:RedEtherbloom/nvf/feature-language-tex";
+      url = "github:isaacST08/nvf/feature-language-tex";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         flake-utils.follows = "flake-utils";
