55d03c669192f45e71cfe5d8feeeae0df7a29f95
RedEtherbloom <etherbloom+github@mailbox.org>
2025-04-02 18:29:38 +0200

    fractor: enable docker

--- hosts/fractor/configuration.nix
+++ hosts/fractor/configuration.nix
@@ -155,6 +155,20 @@
     LIBVA_DRIVER_NAME = "iHD";
   };
 
+  virtualisation = {
+    containers.enable = true;
+    podman = {
+      enable = true;
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+    oci-containers = {
+      backend = "podman";
+    };
+  };
+
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
