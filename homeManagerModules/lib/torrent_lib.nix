ce3a8b1114b84faa01be487aa1d742d643741e62
RedEtherbloom <etherbloom+github@mailbox.org>
2025-07-10 16:36:09 +0200

    kscreen: make layout options failure proof
    Missing outputs now simply get ignored.

---
+++ homeManagerModules/lib/torrent_lib.nix
@@ -0,0 +1,55 @@
+{
+  pkgs,
+  osConfig,
+  ...
+}: let
+  mullvad-torrent = pkgs.writeShellApplication {
+    name = "mullvad-torrent";
+    runtimeInputs = with pkgs; [
+      curl
+      jq
+      qbittorrent
+      flaresolverr
+      jackett
+      # TODO: Try prowlarr instead(more modern, potentially faster)
+    ];
+    text = ''
+       PGID="$(ps -o pgid= $$)"
+       trap 'pkill -15 -$PGID' SIGTERM
+
+       MULLVAD_CONNECTED="$(curl -s https://am.i.mullvad.net/json | jq -e '.mullvad_exit_ip==true' > /dev/null; echo -n "$?")"
+       if [[ "$MULLVAD_CONNECTED" -eq 0 ]]; then
+         echo "Successfully connected to Mullvad"
+       else
+         echo "Could not connect to Mullvad"
+         exit "$MULLVAD_CONNECTED"
+       fi
+
+      qbittorrent &
+      HOST="127.0.0.1" TZ="${osConfig.time.timeZone}" LANG="en_US" flaresolverr &
+      jackett
+    '';
+  };
+  vopono-torrent = pkgs.writeShellApplication {
+    name = "vopono-torrent";
+    runtimeInputs = with pkgs; [
+      vopono
+      mullvad-torrent
+    ];
+    text = ''
+      # qbittorrent is 8180
+      # flaresolverr is 8191
+      # jacket is 9117
+      vopono exec \
+        --provider mullvad \
+        --server germany \
+        --protocol wireguard \
+        -f 8180 \
+        -f 8191 \
+        -f 9117 \
+        mullvad-torrent
+    '';
+  };
+in {
+  inherit mullvad-torrent vopono-torrent;
+}
--- homeManagerModules/roles/nvim.nix
+++ homeManagerModules/roles/nvim.nix
@@ -1,3 +1,4 @@
+# TODO: Move to programs
 {
   config,
   inputs,
