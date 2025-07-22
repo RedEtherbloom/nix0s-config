5922999bbdcfd2c72382985dd96dbb3961d48221
RedEtherbloom <etherbloom@mailbox.org>
2025-07-22 15:16:43 +0200

    kde: remove duplicated default config

--- modules/roles/plasma.nix
+++ modules/roles/plasma.nix
@@ -13,16 +13,5 @@ in {
       desktopManager.plasma6.enableQt5Integration = true;
     };
     environment.systemPackages = [kclock];
-    security.pam.services = {
-      login.kwallet = {
-        enable = true;
-        package = lib.mkDefault pkgs.kdePackages.kwallet-pam;
-      };
-      # Copied from: https://github.com/NixOS/nixpkgs/issues/258296
-      kde = {
-        allowNullPassword = true;
-        kwallet.enable = true;
-      };
-    };
   };
 }
