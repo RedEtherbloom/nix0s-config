18e2f477c0a73bbb8de6f4a4b11f5d91f092bbe1
RedEtherbloom <etherbloom@mailbox.org>
2025-07-22 15:28:12 +0200

    doc: add doc comments

--- modules/common/zsh.nix
+++ modules/common/zsh.nix
@@ -2,6 +2,7 @@
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
 
-  # Supposedly required for e.g. systemd autocompletion
+  # TODO: Why do systemd completions lag so much?
+  # See: https://home-manager-options.extranix.com/?query=programs.zsh.enableCompletion&release=master
   environment.pathsToLink = ["/share/zsh"];
 }
