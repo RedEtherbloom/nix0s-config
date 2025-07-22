7275e6c150b7bc1eb468e4b77bec3322818014db
RedEtherbloom <etherbloom@mailbox.org>
2025-07-22 13:40:59 +0200

    vars: add function for hashing directory
    Was used in a prototype. Kscreenlocker didn't work as iirc the original
    derivation was used, as no version change.

--- homeManagerModules/variables.nix
+++ homeManagerModules/variables.nix
@@ -21,6 +21,10 @@
         kitty zsh -c "cd ${location} && git pull && notify-send \"Sync worked without problem for location $(basename ${location})\" --expire-time=2000 || lazygit"
       '';
     };
+  hash_directory = directory: allowed_extensions: pkgs: (builtins.hashString "sha256"
+    (pkgs.lib.concatMapStrings (file: pkgs.lib.fileContents file)
+      (pkgs.lib.lists.filter (el: pkgs.lib.lists.any (ext: pkgs.lib.strings.hasSuffix ext el) allowed_extensions)
+        (pkgs.lib.filesystem.listFilesRecursive directory))));
 in {
   xdg.dataFile."${builtins.baseNameOf own-hm-data-directory}/.keep".text = "";
 
