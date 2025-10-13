b3e8ac626ca35e4909665e866968262e714ccf2c
RedEtherbloom <etherbloom@mailbox.org>
2025-10-14 01:18:11 +0200

    nix: improve artifact experience

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -1,11 +1,9 @@
 {
   lib,
   config,
-  inputs,
   pkgs,
   ...
-}:
-with lib; let
+}: let
   cfg = config.myOptions.common;
 in {
   imports = [
@@ -17,14 +15,14 @@ in {
   ];
 
   options.myOptions.common = {
-    enableBoot = mkOption {
+    enableBoot = lib.mkOption {
       description = "Enable common bootloader support";
-      type = types.bool;
+      type = lib.types.bool;
       default = true;
     };
   };
 
-  config = mkMerge [
+  config = lib.mkMerge [
     {
       nix = {
         settings = {
@@ -37,65 +35,62 @@ in {
           trusted-users = ["root" "@wheel" "inf"];
           keep-outputs = true;
           keep-derivations = true;
-          # Cache tars for two days to improve dev experience
-          tarball-ttl = 2 * 24 * 3600;
+          # Cache tars for seven days to improve dev experience
+          tarball-ttl = 7 * 24 * 3600;
         };
         gc = {
           automatic = true;
           dates = "daily";
-          options = "--delete-older-than 14d";
+          options = "--delete-older-than 5d";
         };
         optimise = {
           automatic = true;
-          dates = [
-            "15:00"
-          ];
+          dates = ["15:00"];
         };
       };
       programs.nix-ld.enable = true;
       hardware.i2c.enable = true;
       sops.age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
     }
-    (mkIf cfg.enableBoot {
-      boot.kernelPackages = lib.mkOverride 1001 pkgs.linuxPackages_latest;
-      boot.loader = {
-        efi.canTouchEfiVariables = true;
-        timeout = 2;
-        grub = lib.mkDefault {
-          enable = true;
-          enableCryptodisk = true;
-          efiSupport = true;
-          copyKernels = true;
-          fsIdentifier = "uuid";
-          useOSProber = true;
-          device = "nodev";
-
-          memtest86.enable = true;
-          extraEntries = ''
-            menuentry "Poweroff" {
-              halt
-            }
-
-            menuentry "Reboot" {
-              reboot
-            }
-
-            menuentry "UEFI Setup" {
-              fwsetup
-            }
-          '';
+    (lib.mkIf cfg.enableBoot {
+      boot = {
+        kernelPackages = lib.mkOverride 1001 pkgs.linuxPackages_latest;
+        loader = {
+          efi.canTouchEfiVariables = true;
+          timeout = 2;
+          grub = lib.mkDefault {
+            enable = true;
+            enableCryptodisk = true;
+            efiSupport = true;
+            copyKernels = true;
+            fsIdentifier = "uuid";
+            useOSProber = true;
+            device = "nodev";
+            memtest86.enable = true;
+            extraEntries = ''
+              menuentry "Poweroff" {
+                halt
+              }
+              menuentry "Reboot" {
+                reboot
+              }
+              menuentry "UEFI Setup" {
+                fwsetup
+              }
+            '';
+          };
         };
+        # Required for plymouth to work in luks
+        initrd.systemd.enable = true;
+        plymouth.enable = lib.mkDefault true;
+
+        tmp.cleanOnBoot = true;
       };
       # Generate a second, much more verbose boot entry
       # specialisation.verbose-boot.configuration = {
       #   boot.consoleLogLevel = 7;
       #   boot.plymouth.enable = false;
       # };
-
-      # Plymouth keeps crashing
-      # Required for plymouth to work in luks
-      boot.initrd.systemd.enable = true;
-      boot.plymouth.enable = lib.mkDefault true;
     })
   ];
 }
