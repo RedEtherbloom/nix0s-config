8f8c0a001f8e6fdefc98d6e8b25a755c5c03cf94
RedEtherbloom <etherbloom@mailbox.org>
2025-10-16 14:20:46 +0200

    plasma: enable verbose debugging to figure out crashes

--- homeManagerModules/hostRoles/neural-augmenter.nix
+++ homeManagerModules/hostRoles/neural-augmenter.nix
@@ -141,7 +141,9 @@ in {
       sessionVariables = {
         # Smooth scrolling
         MOZ_USE_XINPUT2 = "1";
-        QT_LOGGING_RULES = "kscreenlocker.debug=true";
+        #QT_LOGGING_RULES = "kscreenlocker.debug=true;kwin_*.debug=true;plasma*.debug=true";
+        QT_LOGGING_RULES = "*.debug=true";
+
       };
       extraOutputsToInstall = [
         "doc"
