96e75256dd7bcd35e3d92b3dd316dcea0070cdee
RedEtherbloom <etherbloom@mailbox.org>
2025-10-17 10:17:04 +0200

    stylix: manage fonts again

--- modules/hostRoles/graphical.nix
+++ modules/hostRoles/graphical.nix
@@ -51,14 +51,28 @@ in {
           monospace = ["OpenDyslexicM Nerd Font Mono"];
           emoji = ["Noto Color Emoji"];
           # OpenDyslexic is very large by default. Too large for our taste.
-          # TODO: No clear equivalent without stylix
-          # sizes = {
-          #   applications = 10;
-          #   desktop = 8;
-          #   popups = 8;
-          #   terminal = 10;
-          # };
         };
+      };
+    };
+    stylix.fonts = {
+      monospace = {
+        # TODO: Need a better mono font
+        package = pkgs.nerd-fonts.open-dyslexic;
+        name = "OpenDyslexicM Nerd Font Mono";
+      };
+      serif = {
+        package = pkgs.nerd-fonts.open-dyslexic;
+        name = "OpenDyslexic Nerd Font";
+      };
+      sansSerif = {
+        package = pkgs.nerd-fonts.open-dyslexic;
+        name = "OpenDyslexic Nerd Font";
+      };
+      sizes = {
+        applications = 10;
+        desktop = 8;
+        popups = 8;
+        terminal = 10;
       };
     };
   };
