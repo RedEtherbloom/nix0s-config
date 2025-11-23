4aa3b1256f14069c83eb8181973c7b725c584262
RedEtherbloom <etherbloom@mailbox.org>
2025-11-23 22:37:24 +0100

    neurodrive: attempt to quiet hdds

--- modules/hdd.nix
+++ modules/hdd.nix
@@ -14,7 +14,8 @@
 
   # Spin HDDs down when inactive
   # Taken from: https://www.reddit.com/r/NixOS/comments/751i5t/comment
+  # WE really need more quiet drives
   powerManagement.powerUpCommands = with pkgs; ''
-    ${bash}/bin/bash -c '${hdparm}/bin/hdparm -S 9 -B 127 $(${utillinux}/bin/lsblk -dnp -o name,rota |${gnugrep}/bin/grep ".*\s1" |${coreutils}/bin/cut -d " " -f 1)'
+    ${bash}/bin/bash -c '${hdparm}/bin/hdparm -S 9 -B 20 $(${utillinux}/bin/lsblk -dnp -o name,rota |${gnugrep}/bin/grep ".*\s1" |${coreutils}/bin/cut -d " " -f 1)'
   '';
 }
