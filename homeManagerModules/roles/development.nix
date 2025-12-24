16ab481fe06ebf93fcb05b53a3a28415372ff073
RedEtherbloom <etherbloom@mailbox.org>
2025-12-24 20:59:24 +0100

    feat: install jj utils

--- homeManagerModules/roles/development.nix
+++ homeManagerModules/roles/development.nix
@@ -4,9 +4,11 @@
   osConfig,
   pkgs,
   ...
-}: let
+}:
+let
   cfg = config.myOptions.roles.development;
-in {
+in
+{
   options.myOptions.roles.development = {
     enable = lib.mkOption {
       type = with lib.types; bool;
@@ -83,6 +85,11 @@ in {
       default = true;
       description = "Useful extensions and tools for github.";
     };
+    jj = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Jj and accompanying defaults.";
+    };
     copilot = lib.mkOption {
       type = lib.types.bool;
       default = true;
@@ -116,157 +123,194 @@ in {
     };
   };
 
-  config = lib.mkIf cfg.enable (lib.mkMerge [
-    {
-      home.packages = with pkgs;
-        lib.optionals cfg.rust [
-          clang
-          clang-tools
-          pkg-config
-          (fenix.complete.withComponents [
-            "cargo"
-            "clippy"
-            "rust-src"
-            "rustc"
-            "rustfmt"
-          ])
-          rust-analyzer-nightly
-        ]
-        ++ lib.optionals cfg.openscad [
-          openscad-unstable
-        ]
-        # ++ lib.optionals cfg.nix self.devShells.${system}.default.buildInputs
-        ++ lib.optionals cfg.electronics [
-          kicad
-        ]
-        ++ lib.optionals cfg.three-d-printing [
-          prusa-slicer
-        ]
-        ++ lib.optionals cfg.reverseEngineering [
-          ghidra
-        ]
-        ++ lib.optionals cfg.network-analysis [
-          nmap
-          wireshark
-        ]
-        ++ lib.optionals cfg.python [
-          python3Packages.flake8
-        ]
-        ++ lib.optionals cfg.git [
-          git
-          git-lfs
-          git-filter-repo
-        ]
-        ++ lib.optionals cfg.mcu [
-          # Broken as of 01.08.2025
-          # esphome
-          # platformio
-          esptool
-        ]
-        ++ lib.optionals cfg.cursor [
-          code-cursor
-        ];
-    }
-    (lib.mkIf cfg.git {
-      programs = {
-        git = {
-          enable = true;
-          settings = {
-            user = {
-              name = "RedEtherbloom";
-              email = "etherbloom@mailbox.org";
+  config = lib.mkIf cfg.enable (
+    lib.mkMerge [
+      {
+        home.packages =
+          with pkgs;
+          lib.optionals cfg.rust [
+            clang
+            clang-tools
+            pkg-config
+            (fenix.complete.withComponents [
+              "cargo"
+              "clippy"
+              "rust-src"
+              "rustc"
+              "rustfmt"
+            ])
+            rust-analyzer-nightly
+          ]
+          ++ lib.optionals cfg.openscad [
+            openscad-unstable
+          ]
+          ++ lib.optionals cfg.nix (
+            with pkgs;
+            [
+
+              alejandra
+              nh
+              nixd
+              direnv
+              nix-prefetch-scripts
+              nix-prefetch-github
+              nix-tree
+              (nixos-rebuild-ng.overrideAttrs (
+                _: prevAttrs: {
+                  # "Fixes" conflict with hm zsh completion collission
+                  # fixupPhase = (prevAttrs.fixupPhase or "") + ''
+                  #   rm $out/share/zsh/site-functions/_nixos-rebuild
+                  # '';
+                  meta.priority = 1;
+                }
+              ))
+            ]
+          )
+          # ++ lib.optionals cfg.nix self.devShells.${system}.default.buildInputs
+          ++ lib.optionals cfg.electronics [
+            kicad
+          ]
+          ++ lib.optionals cfg.three-d-printing [
+            prusa-slicer
+          ]
+          ++ lib.optionals cfg.reverseEngineering [
+            ghidra
+          ]
+          ++ lib.optionals cfg.network-analysis [
+            nmap
+            wireshark
+          ]
+          ++ lib.optionals cfg.python [
+            python3Packages.flake8
+          ]
+          ++ lib.optionals cfg.git [
+            git
+            git-lfs
+            git-filter-repo
+          ]
+          ++ lib.optionals cfg.mcu [
+            # Broken as of 01.08.2025
+            # esphome
+            # platformio
+            esptool
+          ]
+          ++ lib.optionals cfg.cursor [
+            code-cursor
+          ];
+      }
+      (lib.mkIf cfg.git {
+        programs = {
+          git = {
+            enable = true;
+            settings = {
+              user = {
+                name = "RedEtherbloom";
+                email = "etherbloom@mailbox.org";
+              };
+              push.autoSetupRemote = true;
             };
-            push.autoSetupRemote = true;
           };
+          lazygit.enable = true;
         };
-        lazygit.enable = true;
-      };
-    })
-    (lib.mkIf cfg.github {
-      programs = {
-        gh-dash.enable = true;
-        gh.enable = true;
-      };
-    })
-    (lib.mkIf cfg.java {
-      programs.java = {
-        enable = true;
-        package = pkgs.jdk25;
-      };
-      home.packages = [pkgs.jetbrains.idea];
-    })
-    (lib.mkIf cfg.direnv {
-      programs.direnv = {
-        enable = true;
-        nix-direnv.enable = true;
-      };
-      systemd.user = {
-        services.gcNixDirenv = {
-          Unit.Description = "Clean up stale or old nix-direnv shells. Script by DrRuhe.";
-          Service = let
-            gcNixDirenv = pkgs.writers.writeNu "gcNixDirenv" ''
-              use std log
+      })
+      (lib.mkIf cfg.github {
+        programs = {
+          gh-dash.enable = true;
+          gh.enable = true;
+        };
+      })
+      (lib.mkIf cfg.jj {
+        programs.jjui = {
+          enable = true;
+        };
+        home.packages = with pkgs; [
+          jujutsu
+          lazyjj
+          jj-fzf
+        ];
+      })
+      (lib.mkIf cfg.java {
+        programs.java = {
+          enable = true;
+          package = pkgs.jdk25;
+        };
+        home.packages = [ pkgs.jetbrains.idea ];
+      })
+      (lib.mkIf cfg.direnv {
+        programs.direnv = {
+          enable = true;
+          nix-direnv.enable = true;
+        };
+        systemd.user = {
+          services.gcNixDirenv = {
+            Unit.Description = "Clean up stale or old nix-direnv shells. Script by DrRuhe.";
+            Service =
+              let
+                gcNixDirenv = pkgs.writers.writeNu "gcNixDirenv" ''
+                  use std log
 
-              def nixStoreGetDevshellGcRoots [] {
-                  return (nix-store --gc --print-roots |
-                      lines |
-                      parse "{loc} -> {storepath}" |
-                      insert dir {|gcRoot|$gcRoot.loc|parse "{path}/.direnv/{x}" | get -i 0.path} |
-                      group-by --to-table dir |
-                      rename dir gcRoots |
-                      insert modified {|devShell|
-                          $devShell.gcRoots|each {|gcRoot|
-                              ls -D $gcRoot.loc|get 0.modified
-                          }|sort --reverse|first
-                      })
-              }
-
-              def removeGCRootsFromDevshells [] {
-                  let devshells = $in
-
-                  $devshells|each {|devshell|
-                      log info $"(ansi red_bold)Removing devshell last modified ($devshell.modified|date humanize): ($devshell.dir) (ansi reset)"
-                      direnv revoke $devshell.dir
-                      rm -r $"($devshell.dir)/.direnv"
+                  def nixStoreGetDevshellGcRoots [] {
+                      return (nix-store --gc --print-roots |
+                          lines |
+                          parse "{loc} -> {storepath}" |
+                          insert dir {|gcRoot|$gcRoot.loc|parse "{path}/.direnv/{x}" | get -i 0.path} |
+                          group-by --to-table dir |
+                          rename dir gcRoots |
+                          insert modified {|devShell|
+                              $devShell.gcRoots|each {|gcRoot|
+                                  ls -D $gcRoot.loc|get 0.modified
+                              }|sort --reverse|first
+                          })
                   }
 
-                  return
-              }
+                  def removeGCRootsFromDevshells [] {
+                      let devshells = $in
+
+                      $devshells|each {|devshell|
+                          log info $"(ansi red_bold)Removing devshell last modified ($devshell.modified|date humanize): ($devshell.dir) (ansi reset)"
+                          direnv revoke $devshell.dir
+                          rm -r $"($devshell.dir)/.direnv"
+                      }
+
+                      return
+                  }
 
 
-              def main [--delete-older-than : duration = 31day] {
-                  nixStoreGetDevshellGcRoots|where modified < ((date now) - $delete_older_than)|removeGCRootsFromDevshells
-              }
-            '';
-          in {
-            Type = "exec";
-            ExecStart = "${gcNixDirenv} --delete-older-than 7day";
+                  def main [--delete-older-than : duration = 31day] {
+                      nixStoreGetDevshellGcRoots|where modified < ((date now) - $delete_older_than)|removeGCRootsFromDevshells
+                  }
+                '';
+              in
+              {
+                Type = "exec";
+                ExecStart = "${gcNixDirenv} --delete-older-than 7day";
+              };
+          };
+          timers.gcNixDirenv = {
+            Timer = {
+              OnCalendar = "weekly";
+              Persistent = true;
+            };
           };
         };
-        timers.gcNixDirenv = {
-          Timer = {
-            OnCalendar = "weekly";
-            Persistent = true;
+      })
+      (lib.mkIf cfg.go {
+        programs.go.enable = true;
+      })
+      (lib.mkIf osConfig.security.ownAdditional.yubikey {
+        # TODO: Maybe merge with github config
+        programs.git = {
+          signing = {
+            format = "openpgp";
+            # TODO: Turn into it's own global variable
+            key = "341EB1EADB36EC0AC809FBE7BA719C19A950A2F3";
+            signByDefault = lib.mkDefault true;
           };
         };
-      };
-    })
-    (lib.mkIf cfg.go {
-      programs.go.enable = true;
-    })
-    (lib.mkIf osConfig.security.ownAdditional.yubikey {
-      # TODO: Maybe merge with github config
-      programs.git = {
-        signing = {
-          format = "openpgp";
-          # TODO: Turn into it's own global variable
-          key = "341EB1EADB36EC0AC809FBE7BA719C19A950A2F3";
-          signByDefault = lib.mkDefault true;
-        };
-      };
-    })
-    (lib.mkIf cfg.helix {
-      programs.helix.enable = true;
-    })
-  ]);
+      })
+      (lib.mkIf cfg.helix {
+        programs.helix.enable = true;
+      })
+    ]
+  );
 }
