8dacdea9765324b840fdde177ee8f16bd684572d
RedEtherbloom <etherbloom@mailbox.org>
2026-01-03 18:24:21 +0100

    hyprsunset: setup times

--- homeManagerModules/programs/hyprland.nix
+++ homeManagerModules/programs/hyprland.nix
@@ -805,7 +805,24 @@ in
       enable = true;
       saveLocation = "$HOME/Pictures/Screenshots";
     };
-    services.hyprsunset.enable = true;
+    services.hyprsunset = {
+      enable = true;
+      settings = {
+        max-gamma = 150;
+
+        profile = [
+          {
+            time = "7:30";
+            identity = true;
+          }
+          {
+            time = "21:00";
+            temperature = 5000;
+            gamma = 0.8;
+          }
+        ];
+      };
+    };
 
     # TODO: Remove unneedded
     home.packages = with pkgs; [
