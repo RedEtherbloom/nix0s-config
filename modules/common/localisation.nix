55216026dbc37c438a14d9bb824a85b32f31e269
RedEtherbloom <etherbloom@mailbox.org>
2026-01-31 20:09:56 +0100

    console: use colemak layout

--- modules/common/localisation.nix
+++ modules/common/localisation.nix
@@ -1,29 +1,26 @@
 {
   time.timeZone = "Europe/Berlin";
-
-  i18n.defaultLocale = "en_IE.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "de_DE.UTF-8";
-    LC_IDENTIFICATION = "de_DE.UTF-8";
-    LC_MEASUREMENT = "de_DE.UTF-8";
-    LC_MONETARY = "de_DE.UTF-8";
-    LC_NAME = "de_DE.UTF-8";
-    LC_NUMERIC = "de_DE.UTF-8";
-    LC_PAPER = "de_DE.UTF-8";
-    LC_TELEPHONE = "de_DE.UTF-8";
-    # Abbreviated weekdays at 3 letters
-    LC_TIME = "en_IE.UTF-8";
+  i18n = {
+    defaultLocale = "en_IE.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "de_DE.UTF-8";
+      LC_IDENTIFICATION = "de_DE.UTF-8";
+      LC_MEASUREMENT = "de_DE.UTF-8";
+      LC_MONETARY = "de_DE.UTF-8";
+      LC_NAME = "de_DE.UTF-8";
+      LC_NUMERIC = "de_DE.UTF-8";
+      LC_PAPER = "de_DE.UTF-8";
+      LC_TELEPHONE = "de_DE.UTF-8";
+      # Abbreviated weekdays at 3 letters
+      LC_TIME = "en_IE.UTF-8";
+    };
   };
 
-  # TODO: change to colemak
   services.xserver.xkb = {
     model = "pc104";
     layout = "us,de";
     variant = "colemak_dh_iso,nodeadkeys";
-    options = "terminate:ctrl_alt_bksp,caps:escape,shift:both_capslock";
+    options = "terminate:ctrl_alt_bksp,caps:escape";
   };
-  # Affects LUKS unlock
-  # TODO: change to colemak
-  # Should get generated using xkb
-  # console.keyMap = "de";
+  console.keyMap = "colemak/mod-dh-iso-us";
 }
