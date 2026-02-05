34926de843151c3ea3f598a15fc031068f39ef51
RedEtherbloom <etherbloom@mailbox.org>
2026-02-05 17:52:40 +0100

    pipewire: raise debug level

--- homeManagerModules/hostRoles/neural-augmenter.nix
+++ homeManagerModules/hostRoles/neural-augmenter.nix
@@ -182,6 +182,7 @@ in {
         MOZ_USE_XINPUT2 = "1";
         #QT_LOGGING_RULES = "kscreenlocker.debug=true;kwin_*.debug=true;plasma*.debug=true";
         #QT_LOGGING_RULES = "*.debug=true";
+        PIPEWIRE_DEBUG = 2; # Print warnings and errors
       };
       extraOutputsToInstall = [
         "doc"
