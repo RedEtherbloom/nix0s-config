a02962af6421d71c0d5a228a1f3701613d8b216c
RedEtherbloom <etherbloom@mailbox.org>
2026-03-12 13:37:45 +0100

    refactor: taskwarrior

--- modules/taskwarrior-secrets.nix
+++ modules/taskwarrior-secrets.nix
@@ -1,28 +1,23 @@
-# TODO: Move to home-managr once it supports sops-nix templates
 {
   config,
   secrets,
   ...
 }: let
-  functions = import ./functions.nix {inherit config;};
+  functions = import ./common/functions.nix {inherit config;};
   taskwarrior_secrets = "${secrets}/secrets/services/taskwarrior.yaml";
 in {
-  # TODO: How do I group these?
-  sops.secrets.encryption_secret = {
-    sopsFile = taskwarrior_secrets;
-  };
-  sops.secrets.client_id = {
-    sopsFile = taskwarrior_secrets;
-  };
-
-  # TODO: Can other users read this by default as well? I hope not
-  # TODO: Use vars.data-server-ip once we have refactored variables.nix
-  sops.templates."taskwarrior-sync.rc" = {
-    owner = "inf";
-    content = ''
-      sync.encryption_secret = ${config.sops.placeholder.encryption_secret}
-      sync.server.client_id = ${config.sops.placeholder.client_id}
-      sync.server.url = http://${functions.data-server-ip}:${toString config.myOptions.services.taskchampion.taskchampionPort}
-    '';
+  sops = {
+    secrets = {
+      encryption_secret.sopsFile = taskwarrior_secrets;
+      client_id.sopsFile = taskwarrior_secrets;
+    };
+    templates."taskwarrior-sync.rc" = {
+      owner = "inf";
+      content = ''
+        sync.encryption_secret = ${config.sops.placeholder.encryption_secret}
+        sync.server.client_id = ${config.sops.placeholder.client_id}
+        sync.server.url = http://${functions.data-server-ip}:${toString config.myOptions.services.taskchampion.taskchampionPort}
+      '';
+    };
   };
 }
