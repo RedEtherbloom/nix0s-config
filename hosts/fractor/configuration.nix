e3edc0cca97fad4a1fb021dce467573c98f7113f
RedEtherbloom <etherbloom@mailbox.org>
2026-06-19 23:48:55 +0200

    refactor: attempt to make unlocking easier

--- hosts/fractor/configuration.nix
+++ hosts/fractor/configuration.nix
@@ -32,7 +32,7 @@
         device = "/dev/disk/by-uuid/7da6adea-a5ff-4044-bd33-38decf43fd60";
         # Needs to be enrolled with systemd-cryptenroll, with sudo systemd-cryptenroll --fido2-with-client-pin=true --fido2-device=auto <disk id>
         crypttabExtraOpts = [
-					"fido2-device=auto"
+					# "fido2-device=auto"
 					"token-timeout=10s"
 				];
         bypassWorkqueues = true;
