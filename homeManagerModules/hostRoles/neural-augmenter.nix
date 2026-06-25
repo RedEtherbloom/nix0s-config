a77c47f9aa037a79779d225b3d0a39a703c444b7
RedEtherbloom <etherbloom@mailbox.org>
2026-06-25 03:05:29 +0200

    refactor: remove unused fonts

--- homeManagerModules/hostRoles/neural-augmenter.nix
+++ homeManagerModules/hostRoles/neural-augmenter.nix
@@ -159,12 +159,7 @@ in
             powerline-symbols
             powerline-fonts
             noto-fonts-color-emoji # fcitx5
-            gyre-fonts
-            fira-sans
             nerd-fonts.fira-code
-            nerd-fonts.fira-mono
-
-            # TODO: Find a file manager with vim keybinds
 
             (pkgs.writeShellScriptBin "rofi-home-assistant-sops.sh" ''
               set -e
