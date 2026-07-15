3ad3bd8abd4958fdeb8ee74eaf4be8ae7959854e
RedEtherbloom <etherbloom@mailbox.org>
2026-07-15 19:29:09 +0200

    fractor: force normal scaling

--- hosts/fractor/home.nix
+++ hosts/fractor/home.nix
@@ -1,11 +1,14 @@
-{pkgs, ...}: {
+{ pkgs, ... }: {
   imports = [
     ../../homeManagerModules
   ];
 
-  home.stateVersion = "24.05";
+  home = {
+    stateVersion = "24.05";
+    packages = with pkgs; [
+      aircrack-ng
+    ];
+  };
 
-  home.packages = with pkgs; [
-    aircrack-ng
-  ];
+  programs.niri.settings.outputs."eDP-1".scale = 1.0;
 }
